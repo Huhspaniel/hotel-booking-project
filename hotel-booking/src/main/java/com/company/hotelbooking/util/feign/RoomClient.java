43eb3de6b3e476124e3604140b13acbff981cd7e
Jordan Senken <jsenken@yahoo.com>
2019-10-07 16:39:35 -0400

    adding service layer to master

--- hotel-booking/src/main/java/com/company/hotelbooking/util/feign/RoomClient.java
+++ hotel-booking/src/main/java/com/company/hotelbooking/util/feign/RoomClient.java
@@ -2,19 +2,6 @@ package com.company.hotelbooking.util.feign;
 
 import com.company.hotelbooking.model.Room;
 import org.springframework.cloud.openfeign.FeignClient;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Optional;
-
-@FeignClient(name = "room-service")
-public interface RoomClient {
-    @RequestMapping(value="/room/{number}", method = RequestMethod.GET)
-    public Optional<Room> getRoom(@PathVariable Integer number);
-
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -24,5 +11,4 @@ import java.util.Optional;
 public interface RoomClient {
     @GetMapping("/room/{number}")
     public Optional<Room> getRoom(@PathVariable String number);
->>>>>>> b11ee22029e8915440ba5e12bff2a699ce6d81ec
 }
