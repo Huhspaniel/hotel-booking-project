072e728b92a982181223b9e17b7e874f33126e33
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-10-07 16:01:37 -0400

    Add HotelRewardsService and test class

---
+++ hotel-booking/src/main/java/com/company/hotelbooking/service/HotelRewardsService.java
@@ -0,0 +1,25 @@
+package com.company.hotelbooking.service;
+
+import com.company.hotelbooking.util.feign.RewardsClient;
+import com.company.hotelbooking.util.feign.RoomClient;
+import com.company.hotelbooking.viewmodel.HotelRewardsView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class HotelRewardsService {
+    private RewardsClient rewardsClient;
+    private RoomClient roomClient;
+
+    @Autowired
+    public HotelRewardsService(RewardsClient rewardsClient, RoomClient roomClient) {
+        this.rewardsClient = rewardsClient;
+        this.roomClient = roomClient;
+    }
+
+    public Optional<HotelRewardsView> getRewardsInfo(Integer roomId, Boolean rewardsMember, Boolean doubleBonusDay) {
+        return Optional.empty();
+    }
+}
