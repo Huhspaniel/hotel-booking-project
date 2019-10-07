25148a6f5836dfa1fbe52431bae2e812852f94fc
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-10-07 15:46:47 -0400

    Add RoomService

--- hotel-booking/src/main/java/com/company/hotelbooking/util/feign/RewardsClient.java
+++ hotel-booking/src/main/java/com/company/hotelbooking/util/feign/RewardsClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient
+@FeignClient("rewards-service")
 public interface RewardsClient {
     @GetMapping("/rewards/type/{roomType}")
     HotelRewardsView getRewards(@PathVariable String roomType);
