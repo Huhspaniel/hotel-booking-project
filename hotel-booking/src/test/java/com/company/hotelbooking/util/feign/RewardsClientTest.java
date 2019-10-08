605fc756673e2e7f95c34a9c8ea8fe9da8cf2d71
ace <allisterg@gmail.com>
2019-10-07 22:06:44 -0400

    add feign tests

---
+++ hotel-booking/src/test/java/com/company/hotelbooking/util/feign/RewardsClientTest.java
@@ -0,0 +1,32 @@
+package com.company.hotelbooking.util.feign;
+
+import com.company.hotelbooking.model.Rewards;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RewardsClientTest {
+
+    @Autowired
+    RewardsClient rewardsClient;
+
+    @Test
+    public void getRewards() {
+
+        Rewards expectedRewards = new Rewards(1,
+                "double",
+                new BigDecimal("0.2"),
+                200, true);
+
+        Rewards rewardsFromClient = rewardsClient.getRewards("double");
+        assertEquals(expectedRewards, rewardsFromClient);
+    }
+}
---
+++ hotel-booking/src/test/java/com/company/hotelbooking/util/feign/RoomClientTest.java
@@ -0,0 +1,30 @@
+package com.company.hotelbooking.util.feign;
+
+import com.company.hotelbooking.model.Room;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoomClientTest {
+
+    @Autowired
+    RoomClient roomClient;
+
+    @Test
+    public void getRoom() {
+
+        Room expectedRoom = new Room("101","double",new BigDecimal("189.0"));
+
+        Optional<Room> roomFromClient = roomClient.getRoom("101");
+        assertEquals(expectedRoom, roomFromClient.get());
+    }
+}
